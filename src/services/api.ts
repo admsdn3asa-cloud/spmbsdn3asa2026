27f975d071c25824f4dd851752827d0a29d89d2f
admsdn3asa-cloud <adm.sdn3asa@gmail.com>
2026-05-11 20:50:30 +0700

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // Service to interact with Google Apps Script Backend
 
 // To use the real backend, replace this URL with your deployed Google Apps Script Web App URL
-const GAS_WEB_APP_URL = ""; 
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbx-vUMafa0eKtGuVF7rgeL1OuW6sWb4XMdRCCX7HTc4tiSmxba128wtL9YGO7kGqKBonQ/exec"; 
 
 export interface FormField {
   id: string;
